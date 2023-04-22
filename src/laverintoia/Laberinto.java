ec3fad29533f1b992b45e914f26203c68c4450a8
FaspUMG <fsalvadorp1@miumg.edu.gt>
2023-04-22 16:45:05 -0600

    subiendo

---
+++ src/laverintoia/Laberinto.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laverintoia;
+
+/**
+ *
+ * @author fasp9
+ */
+public class Laberinto {
+    
+}
