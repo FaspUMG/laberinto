9de0c190f4cae259c06101814c9b91bb463cba6b
FaspUMG <fsalvadorp1@miumg.edu.gt>
2023-04-29 16:23:11 -0600

    Variables no subidas

---
+++ src/laverintoia/LaberintoPropiedades.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laverintoia;
+
+/**
+ *
+ * @author fasp9
+ */
+public class LaberintoPropiedades {
+    private short estadoInicial[][];
+    private short estadoActual[][];
+    
+    public int devuelveValor(){
+        System.out.println("Evaluaremos el valor y devolveremos el estado del nodo actual.");
+        return 0;
+    }
+    
+}
